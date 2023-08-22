d2b97622986a71d53884ee416412de4ab098efde
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-08-22 09:36:13 -0700

    stars.js

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,14 +2,15 @@ import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-// import CanvasLoader from "../Loader";
+import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={1} groundColor="black" />
+      <pointLight intensity={8} />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -18,11 +19,11 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.6 : 0.65}
+        position={isMobile ? [0, -3.9, -1.8] : [0, -3.83, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -55,14 +56,13 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      {/* fallback={<CanvasLoader />} */}
-      <Suspense >
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,7 +5,8 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+  // The positions attribute of Points have to be an array that length is divisible by 3, so just changing the 5000 count to 6000 (or any value that is divisible by 3) will do the trick. 
+  const [sphere] = useState(() => random.inSphere(new Float32Array(6000), { radius: 1.2 }));
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
