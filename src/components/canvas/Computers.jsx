c3b413190e3f07a9cd6e7a0b5bca88b2da9f043a
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-08-23 01:37:31 -0700

    nnn

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,28 +5,29 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("/planet/scene.gltf");
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
-    <mesh>
-      <hemisphereLight intensity={1} groundColor="black" />
-      {/* <pointLight intensity={2} /> */}
-      {/* <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      /> */}
+    <primitive object={computer.scene} scale={2.5} position-y={0} rotation-y={0} />
+    // <mesh>
+    //   <hemisphereLight intensity={1} groundColor="black" />
+    //   <pointLight intensity={8} />
+    //   <spotLight
+    //     position={[-20, 50, 10]}
+    //     angle={0.12}
+    //     penumbra={1}
+    //     intensity={1}
+    //     castShadow
+    //     shadow-mapSize={1024}
+    //   />
 
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.6 : 0.75}
-        position={isMobile ? [0, -1.9, -1.8] : [0, -1.83, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
+    //   <primitive
+    //     object={computer.scene}
+    //     scale={isMobile ? 0.6 : 0.65}
+    //     position={isMobile ? [0, -3.9, -1.8] : [0, -3.83, -1.5]}
+    //     rotation={[-0.01, -0.2, -0.1]}
+    //   />
+    // </mesh>
   );
 };
 
@@ -56,29 +57,23 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-    shadows
-    frameloop="demand"
-    dpr={[1, 2]}
-    gl={{ preserveDrawingBuffer: true }}
-    camera={{
-      fov: 45,
-      near: 0.1,
-      far: 200,
-      position: [-4, 3, 6],
-    }}
-  >
-    <Suspense fallback={<CanvasLoader />}>
-      <OrbitControls
-        autoRotate
-        enableZoom={false}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
-      />
-      <Computers/>
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
 
       <Preload all />
-    </Suspense>
-  </Canvas>
+    </Canvas>
   );
 };
 
