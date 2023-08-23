aaaf9a2f009b6c4e296e3e196a7d3e82f8fd2917
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-08-23 01:11:57 -0700

    ttt

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Ismail</span>
+            Hi, I'm <span className='text-[#915EFF]'> Yusuf Ismail</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop  interfaces<br className='sm:block hidden' />
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,14 +11,14 @@ const Computers = ({ isMobile }) => {
     <mesh>
       <hemisphereLight intensity={1} groundColor="black" />
       <pointLight intensity={8} />
-      <spotLight
+      {/* <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
         castShadow
         shadow-mapSize={1024}
-      />
+      /> */}
 
       <primitive
         object={computer.scene}
