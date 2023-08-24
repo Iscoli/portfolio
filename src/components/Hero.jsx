8079004639662b8807ec2702ed84e4cb3413f481
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-08-24 01:30:12 -0700

    image changing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,14 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section style={{marginBottom:'100px'}} className={`relative w-full h-screen mx-auto`}
+    <section style={{marginBottom:'130px'}} className={`relative w-full h-screen mx-auto sandi1n `}
     styles={{marginTop:'-23px'}}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
-        // styles={{ height:'150%'}}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5   sandi2`}
+       
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+        <div className='flex flex-col justify-center items-center mt-5  sandi3'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF] ' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
@@ -28,11 +28,13 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      <div className="h-[120%]">
+        <ComputersCanvas />
+      </div>
 
-      <div className='absolute xs:bottom--10 bottom-1 w-full flex justify-center items-center'>
+      <div className='absolute   xs:bottom-5 bottom-5 w-full flex justify-center items-center'>
         <a href='#about'>
-          <div className='w-[30px] h-[58px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <div className='w-[30px] h-[54px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
                 y: [0, 24, 0],
